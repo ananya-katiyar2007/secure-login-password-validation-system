25c046a8cfdff6ba24ae1dd4d39966479cefc79d
Ananya Katiyar <ananya.katiyar2007@gmail.com>
2026-09-21 16:13:23 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,17 @@
+Before: [10, 20, 30, 40]
+After: [10, 20, 30]
+
+Before: hello
+After: hello
+
+Before add_entry: {'a': 1}
+After add_entry: {'a': 1, 'new_key': 'new_value'}
+
+Before reassign_dict: {'a': 1}
+After reassign_dict: {'a': 1}
+
+Max of 12 and 25: 25
+
+Factorial of 5: 120
+
+25°C in Fahrenheit: 77.0
